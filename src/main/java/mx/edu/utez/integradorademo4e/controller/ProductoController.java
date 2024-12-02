f6c4c7273ab4c49a230829d61453e4a8f006c270
El_Actor <20233tn075@utez.edu.mx>
2024-12-01 23:10:43 -0600

    Producto Controller

--- src/main/java/mx/edu/utez/integradorademo4e/controller/ProductoController.java
+++ src/main/java/mx/edu/utez/integradorademo4e/controller/ProductoController.java
@@ -1,4 +1,21 @@
 package mx.edu.utez.integradorademo4e.controller;
 
+import mx.edu.utez.integradorademo4e.entity.Producto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producto")
 public class ProductoController {
+
+
+
+    @PostMapping("/agregarProducto")
+    public boolean agregarProducto(@RequestBody Producto producto) {
+        boolean respuesta = false;
+        return respuesta;
+    }
+
 }
