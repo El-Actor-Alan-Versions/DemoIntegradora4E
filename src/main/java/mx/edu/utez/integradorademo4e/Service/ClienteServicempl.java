83f171d40c304a24a17364998aa5e3fce2c73a19
jonaBarrera <20233tn110@utez.edu.mx>
2024-12-01 23:39:27 -0600

    mi service de cliente

---
+++ src/main/java/mx/edu/utez/integradorademo4e/Service/ClienteServicempl.java
@@ -0,0 +1,21 @@
+package mx.edu.utez.integradorademo4e.Service;
+
+import mx.edu.utez.integradorademo4e.entity.Cliente;
+import mx.edu.utez.integradorademo4e.entity.dao.IClienteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServicempl {
+    @Autowired
+    private IClienteDao iclienteDao;
+
+    public Cliente agregarCliente(Cliente cliente) {
+        try {
+            return iclienteDao.save(cliente);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
