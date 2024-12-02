3fea35944cce550fd718fb692757e2f2d2f208f5
El_Actor <20233tn075@utez.edu.mx>
2024-12-01 23:55:34 -0600

    Metodo para hacer el insert (Implements)

--- src/main/java/mx/edu/utez/integradorademo4e/Service/ProductoServiceImpl.java
+++ src/main/java/mx/edu/utez/integradorademo4e/Service/ProductoServiceImpl.java
@@ -1,12 +1,27 @@
 package mx.edu.utez.integradorademo4e.Service;
 
 import mx.edu.utez.integradorademo4e.entity.Producto;
+import mx.edu.utez.integradorademo4e.entity.dao.IProductoDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductoServiceImpl implements IProductoService{
-    @Override
-    public void crearProducto(Producto producto) {
 
+    @Autowired
+    IProductoDao dao;
+
+    @Override
+    public Producto crearProducto(Producto producto) {
+
+        try{
+            if(producto != null){
+                dao.save(producto);
+            }
+            return producto;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return producto;
     }
 }
