330c85f0850b22bffda1c8f09f88d977924c8761
El_Actor <20233tn075@utez.edu.mx>
2024-12-01 21:33:16 -0600

    primer Commit

---
+++ src/main/java/mx/edu/utez/integradorademo4e/IntegradoraDemo4EApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.integradorademo4e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradoraDemo4EApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradoraDemo4EApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/integradorademo4e/IntegradoraDemo4EApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.integradorademo4e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradoraDemo4EApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
