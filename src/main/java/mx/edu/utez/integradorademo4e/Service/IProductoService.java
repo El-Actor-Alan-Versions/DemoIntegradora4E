6235bc0dfb7b35d6d22acb39ef2aa111138cc4c3
El_Actor <20233tn075@utez.edu.mx>
2024-12-01 23:30:55 -0600

    Producto Service (Crear producto)

---
+++ src/main/java/mx/edu/utez/integradorademo4e/Service/IProductoService.java
@@ -0,0 +1,9 @@
+package mx.edu.utez.integradorademo4e.Service;
+
+import mx.edu.utez.integradorademo4e.entity.Producto;
+
+public interface IProductoService {
+
+    public void crearProducto(Producto producto);
+
+}
