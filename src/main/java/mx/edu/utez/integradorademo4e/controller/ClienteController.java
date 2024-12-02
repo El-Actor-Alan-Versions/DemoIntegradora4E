b4567190d7e5279a76cee743776188b7342c4af0
jonaBarrera <20233tn110@utez.edu.mx>
2024-12-01 23:52:51 -0600

    metodo pa agregar cliente

--- src/main/java/mx/edu/utez/integradorademo4e/controller/ClienteController.java
+++ src/main/java/mx/edu/utez/integradorademo4e/controller/ClienteController.java
@@ -1,4 +1,31 @@
 package mx.edu.utez.integradorademo4e.controller;
 
+import mx.edu.utez.integradorademo4e.Service.ClienteServicempl;
+import mx.edu.utez.integradorademo4e.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
+
+    @Autowired
+    private ClienteServicempl clienteService;
+
+    @PostMapping("/agregarCliente")
+    public ResponseEntity<Cliente> agregarCliente(@RequestBody Cliente cliente) {
+        Cliente nuevoCliente = clienteService.agregarCliente(cliente);
+
+        if (nuevoCliente != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevoCliente);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
 }
+
