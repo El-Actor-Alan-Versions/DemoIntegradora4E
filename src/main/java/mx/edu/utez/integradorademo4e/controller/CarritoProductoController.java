d4b32a73907e1bfb86b6996b6cd351621483b88d
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:36:28 -0600

    Se agrego un private en CarritoProductoController

--- src/main/java/mx/edu/utez/integradorademo4e/controller/CarritoProductoController.java
+++ src/main/java/mx/edu/utez/integradorademo4e/controller/CarritoProductoController.java
@@ -3,8 +3,12 @@ package mx.edu.utez.integradorademo4e.controller;
 import mx.edu.utez.integradorademo4e.service.ICarritoProductoService;
 import mx.edu.utez.integradorademo4e.entity.CarritoProducto;
 import mx.edu.utez.integradorademo4e.service.IClienteService;
+import mx.edu.utez.integradorademo4e.utils.CustomStack;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/carrito")
@@ -14,6 +18,11 @@ public class CarritoProductoController {
     private ICarritoProductoService service;
     @Autowired
     private IClienteService service2;
+
+    private final Map<Long, CustomStack<CarritoProducto>> historialEliminados= new HashMap<>();
+
+
+
 
     @PostMapping("/agregar")// /carrito/agregar
     public CarritoProducto agregar(@RequestBody CarritoProducto carritoProducto) {
