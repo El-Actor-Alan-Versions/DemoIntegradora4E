740ca8a99d253b7202c57ca925c3f6188193e2b6
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:12:41 -0600

    se implemento un metodo en carritoProductoImpl

--- src/main/java/mx/edu/utez/integradorademo4e/service/CarritoProductoServiceImpl.java
+++ src/main/java/mx/edu/utez/integradorademo4e/service/CarritoProductoServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 public class CarritoProductoServiceImpl implements ICarritoProductoService {
 
@@ -18,4 +20,17 @@ public class CarritoProductoServiceImpl implements ICarritoProductoService {
         dao.save(carritoProducto);
         return carritoProducto;
         }
+
+    @Override
+    @Transactional
+    public CarritoProducto eliminarCarritoProducto(Long id) {
+        Optional<CarritoProducto> carritoProducto = dao.findById(id);
+        if (carritoProducto.isPresent()) {
+            dao.deleteById(id);
+            return carritoProducto.get();
+        }
+        return null;
+    }
+
+
 }
