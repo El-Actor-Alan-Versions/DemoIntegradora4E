7c8dae4b1c74f3746f903fc4a4bbd01e6c5c974d
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:32:10 -0600

    Se agrego el List en el ICarritoProductoDao

--- src/main/java/mx/edu/utez/integradorademo4e/entity/dao/ICarritoProductoDao.java
+++ src/main/java/mx/edu/utez/integradorademo4e/entity/dao/ICarritoProductoDao.java
@@ -4,5 +4,9 @@ import mx.edu.utez.integradorademo4e.entity.CarritoProducto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ICarritoProductoDao extends JpaRepository<CarritoProducto, Long> {
+    List<CarritoProducto> findByClienteId(Long clienteId);
+
 }
