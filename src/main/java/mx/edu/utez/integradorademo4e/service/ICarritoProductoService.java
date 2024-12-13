a7c35a32fdfda8cf222399db0544277632664a5c
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:28:13 -0600

    Se agrego el metodo obtenerCarritoPorCliente

--- src/main/java/mx/edu/utez/integradorademo4e/service/ICarritoProductoService.java
+++ src/main/java/mx/edu/utez/integradorademo4e/service/ICarritoProductoService.java
@@ -2,10 +2,12 @@ package mx.edu.utez.integradorademo4e.service;
 
 import mx.edu.utez.integradorademo4e.entity.CarritoProducto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ICarritoProductoService {
     public CarritoProducto addCarritoProducto(CarritoProducto carritoProducto);
     public CarritoProducto eliminarCarritoProducto(Long id);
+    public List<CarritoProducto> obtenerCarritoPorCliente(Long clienteId);
 }
 
