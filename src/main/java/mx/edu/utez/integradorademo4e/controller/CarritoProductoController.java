a2944527ad62818be64fe0c8b0b1be348d60bd9e
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:34:53 -0600

    Se agregaron cosas en CarritoProductoController

--- src/main/java/mx/edu/utez/integradorademo4e/controller/CarritoProductoController.java
+++ src/main/java/mx/edu/utez/integradorademo4e/controller/CarritoProductoController.java
@@ -2,6 +2,7 @@ package mx.edu.utez.integradorademo4e.controller;
 
 import mx.edu.utez.integradorademo4e.service.ICarritoProductoService;
 import mx.edu.utez.integradorademo4e.entity.CarritoProducto;
+import mx.edu.utez.integradorademo4e.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,8 @@ public class CarritoProductoController {
 
     @Autowired
     private ICarritoProductoService service;
+    @Autowired
+    private IClienteService service2;
 
     @PostMapping("/agregar")// /carrito/agregar
     public CarritoProducto agregar(@RequestBody CarritoProducto carritoProducto) {
