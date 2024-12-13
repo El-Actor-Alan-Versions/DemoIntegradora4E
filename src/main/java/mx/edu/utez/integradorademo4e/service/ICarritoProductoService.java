500d4ecd9cad41cc30180a00f871037226e88e8d
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:18:37 -0600

    Se agrego en metodo en ICarritoProductoService

--- src/main/java/mx/edu/utez/integradorademo4e/service/ICarritoProductoService.java
+++ src/main/java/mx/edu/utez/integradorademo4e/service/ICarritoProductoService.java
@@ -5,3 +5,5 @@ import mx.edu.utez.integradorademo4e.entity.CarritoProducto;
 public interface ICarritoProductoService {
     public CarritoProducto addCarritoProducto(CarritoProducto carritoProducto);
 }
+public void limpiarCarrito(Long clienteId) {
+}
