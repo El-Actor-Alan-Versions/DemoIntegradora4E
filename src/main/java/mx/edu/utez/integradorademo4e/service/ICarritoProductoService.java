9a3f124ce41adc1a407adf01d63fe50c051e06bc
CiscoPaquitoTracer <20233tn239@utez.edu.mx>
2024-12-13 00:27:17 -0600

    Se agrego el metodo eliminarProducto en ICarritoProductoService

--- src/main/java/mx/edu/utez/integradorademo4e/service/ICarritoProductoService.java
+++ src/main/java/mx/edu/utez/integradorademo4e/service/ICarritoProductoService.java
@@ -2,8 +2,10 @@ package mx.edu.utez.integradorademo4e.service;
 
 import mx.edu.utez.integradorademo4e.entity.CarritoProducto;
 
+import java.util.Optional;
+
 public interface ICarritoProductoService {
     public CarritoProducto addCarritoProducto(CarritoProducto carritoProducto);
+    public CarritoProducto eliminarCarritoProducto(Long id);
 }
-public void limpiarCarrito(Long clienteId) {
-}
+
